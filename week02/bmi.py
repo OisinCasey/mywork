835bc7a712f8f2f072f9525b41a24e7b78c6e160
Ush Balls <G00171575@gmit.ie>
2021-02-01 16:30:52 +0000

    week 02 -bmi comments

--- week02/bmi.py
+++ week02/bmi.py
@@ -1,5 +1,6 @@
 height = int(input("Please enter your height in cm: "))
 weight = int(input("Please enter your weight in kg: "))
 bmi = weight / height
+#next line allows formatting of output to a string with two figures after the decimal point
 formattedBmi = "{:.2f}".format(bmi)
 print ("BMI is: " + formattedBmi + ".")
